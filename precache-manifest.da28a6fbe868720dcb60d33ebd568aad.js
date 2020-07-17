890c1f29c73d67bf9be46a578d98da1506f1ae19
Jon Gamble <jon.gamble@palmettoventures.com>
2020-07-17 16:46:44 -0400

    Updates

---
+++ precache-manifest.da28a6fbe868720dcb60d33ebd568aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e3f204cd2b1931456636dfdc877782",
+    "url": "/dst-scoring/index.html"
+  },
+  {
+    "revision": "dfe44262a598d9eaca1a",
+    "url": "/dst-scoring/static/css/main.3c0b0232.chunk.css"
+  },
+  {
+    "revision": "b7b855435f395a6d1cd6",
+    "url": "/dst-scoring/static/js/2.c36d0700.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dst-scoring/static/js/2.c36d0700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe44262a598d9eaca1a",
+    "url": "/dst-scoring/static/js/main.f6e29d59.chunk.js"
+  },
+  {
+    "revision": "f2ddc49f89aa4a9a4e72",
+    "url": "/dst-scoring/static/js/runtime-main.324ff371.js"
+  }
+]);
